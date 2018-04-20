c752a8d6b5732f1b3f1b58f039af4d12d950ff34
carlguo2 <32140669+carlguo2@users.noreply.github.com>
2018-04-19 23:00:49 -0500

    Trying to get the player to move

--- final-project-carlguo2/src/Player.cpp
+++ final-project-carlguo2/src/Player.cpp
@@ -1,6 +1,10 @@
 #include "Player.h"
 
 void Player::setup(ofImage* img) {
+	// set player attributes
+	speed = 5;
+
+	// load image of the player
 	player_img = img;
 	width = player_img->getWidth();
 	height = player_img->getWidth();
@@ -21,6 +25,12 @@ void Player::shoot() {
 }
 
 void Player::calculate_movement() {
+	// if 'a' key is pressed, move left of screen (subtract by x)
+	if (is_a_pressed_ && (position.x - width/2) > 0) {
+		// move to the left by speed
+		position.x -= speed;
+	}
+
 
 }
 
